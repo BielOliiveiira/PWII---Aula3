b14c538017b4be6b549de2989db707b6ef6ce668
BielOliiveiira <gabriel_vm@live.com>
2019-08-14 20:30:32 -0300

    Revisao.ts

---
+++ impl/revisao.js
@@ -0,0 +1,5 @@
+var media1 = 10;
+var media2 = 20;
+var media3 = 30;
+var soma = (media1 + media2 + media3) / 3;
+console.log(soma);
---
+++ revisao.ts
@@ -0,0 +1,5 @@
+let media1: number = 10
+let media2: number = 20
+let media3: number = 30
+let soma = (media1 + media2 + media3) / 3;
+console.log(soma)
