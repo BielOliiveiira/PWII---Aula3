1a14b8a2ab43bf11b5999821c4f6ac11684d01d7
BielOliiveiira <gabriel_vm@live.com>
2019-08-14 21:45:46 -0300

    Criação de Função

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,5 +1,8 @@
-var media1 = 10;
-var media2 = 20;
-var media3 = 30;
-var soma = (media1 + media2 + media3) / 3;
-console.log(soma);
+var n1 = 10;
+var n2 = 20;
+var n3 = 30;
+//let soma = (media1 + media2 + media3) / 3;
+var fMedia = function (nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("" + fMedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -1,5 +1,9 @@
-let media1: number = 10
-let media2: number = 20
-let media3: number = 30
-let soma = (media1 + media2 + media3) / 3;
-console.log(soma)
+let n1: number = 10
+let n2: number = 20
+let n3: number = 30
+//let soma = (media1 + media2 + media3) / 3;
+let fMedia = function(nota1:number, nota2:number, nota3:number): number {
+    return (nota1 + nota2 + nota3) / 3
+}
+console.log(`${fMedia(n1, n2, n3)}`)
+
