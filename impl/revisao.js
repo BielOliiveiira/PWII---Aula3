d4a6fb4fa2c76c0e801711291778376520eedee9
BielOliiveiira <gabriel_vm@live.com>
2019-08-14 22:00:00 -0300

    Utilização da função seta

--- impl/revisao.js
+++ impl/revisao.js
@@ -2,7 +2,8 @@ var n1 = 10;
 var n2 = 20;
 var n3 = 30;
 //let soma = (media1 + media2 + media3) / 3;
-var fMedia = function (nota1, nota2, nota3) {
-    return (nota1 + nota2 + nota3) / 3;
-};
+//let fMedia = function(nota1:number, nota2:number, nota3:number): number {
+//   return (nota1 + nota2 + nota3) / 3
+//}
+var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
 console.log("" + fMedia(n1, n2, n3));
