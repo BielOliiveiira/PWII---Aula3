e28448a014b3dbdee35dc356ec7f1706cdd6c7fa
BielOliiveiira <gabriel_vm@live.com>
2019-08-14 22:13:29 -0300

    Valor padrão (função)

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,9 +1,12 @@
 var n1 = 10;
 var n2 = 20;
-var n3 = 30;
+//let n3: number = 30
 //let soma = (media1 + media2 + media3) / 3;
 //let fMedia = function(nota1:number, nota2:number, nota3:number): number {
 //   return (nota1 + nota2 + nota3) / 3
 //}
-var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
-console.log("" + fMedia(n1, n2, n3));
+var fMedia = function (nota1, nota2, nota3) {
+    if (nota3 === void 0) { nota3 = 7; }
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("" + fMedia(n1, n2));
